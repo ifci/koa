41d3ec3cd27286bb87c1e17c4246f01af9d9f80b
fcdcyy <1020358873@qq.com>
2016-03-04 18:21:06 +0800

    2016/3/4 koa 路由

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-'use strict';
-const Koa = require('koa');
-const app = new Koa();
-// const co = require('co');
-const Router = require('koa-66');
-// const router = new Router();
-const mountRouter = new Router();
-const Controller = require('./router');
+/*const koa = require('koa');
+const app = new koa();
+
 app.use((ctx, next) => {
   const start = new Date;
   return next().then(() => {
@@ -13,9 +8,95 @@ app.use((ctx, next) => {
     console.log(`${ctx.method} ${ctx.url} - ${ms}`);
   });
 });
-// app.use((ctx) => {
-//   ctx.body = 'Hello World!';
-// });
-mountRouter.mount('/blog', Controller.router);
-app.use(mountRouter.routes());
+
+app.use((ctx) => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3001);*/
+
+var koa = require('koa');
+var app = new koa();
+
+/*app.use(function *() {
+  this.body = "header\n";
+  yield saveResults.call(this);
+  this.body += "footer\n";
+});
+
+function *saveResults() {
+  this.body += "Results Saved!\n";
+}*/
+
+// 多个中间件的合并
+/*function *random(next) {
+  if ('/random' == this.path) {
+    this.body = Math.floor(Math.random()*10);
+  } else {
+    yield next;
+  }
+};
+
+function *backwards(next) {
+  if ('/backwards' == this.path) {
+    this.body = 'sdrawkcab';
+  } else {
+    yield next;
+  }
+}
+
+function *pi(next) {
+  if ('/pi' == this.path) {
+    this.body = String(Math.PI);
+  } else {
+    yield next;
+  }
+}
+
+function *all(next) {
+  yield random.call(this, backwards.call(this, pi.call(this, next)));
+}
+
+app.use(all);*/
+
+// 路由
+// 多路径
+// normal route
+/*app.use(function* (next) {
+  if (this.path !== '/') {
+    return yield next
+  }
+
+  this.body = 'hello world'
+});
+
+// /404 route
+app.use(function* (next) {
+  if (this.path !== '/404') {
+    return yield next;
+  }
+
+  this.body = 'page not found'
+});
+
+// /500 route
+app.use(function* (next) {
+  if (this.path !== '/500') {
+    return yield next;
+  }
+
+  this.body = 'internal server error'
+});*/
+
+// 使用koa-router
+var Router = require('koa-router');
+
+var myRouter = new Router();
+
+myRouter.get('/', function *(next) {
+  this.response.body = 'Hello World!';
+});
+
+app.use(myRouter.routes());
+
 app.listen(3001);
